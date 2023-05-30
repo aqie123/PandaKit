3e2d14b8d7c24890ad99b79b3fff9edf3a785a43
aqie <2924811900@qq.com>
2023-05-30 18:03:18 +0800

    page struct

--- model/page.go
+++ model/page.go
@@ -13,3 +13,20 @@ type ResultPage struct {
 	PageSize int64 `json:"pageSize"`
 	Data     any   `json:"data"`
 }
+
+// PageReq 分页
+type PageReq struct {
+	PageNum  int `json:"pageNum"  example:"10" d:"1" v:"min:1#页码最小值不能低于1"  dc:"当前页码" form:"pageNum"`
+	PageSize int `json:"pageSize" example:"1" d:"10" v:"min:1|max:200#每页数量最小值不能低于1|最大值不能大于200" dc:"每页数量" form:"pageSize"`
+}
+
+// RangeDateReq 时间范围查询
+type RangeDateReq struct {
+	StartTime string `json:"start_time" v:"date#开始日期格式不正确"  dc:"开始日期" form:"startTime"`
+	EndTime   string `json:"end_time" v:"date#结束日期格式不正确" dc:"结束日期" form:"endTime"`
+}
+
+// StatusReq 通用状态查询
+type StatusReq struct {
+	Status int `json:"status"  v:"in:-1,0,1,2,3#输入的状态是无效的" dc:"状态" form:"status"`
+}
